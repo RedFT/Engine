85d4855c31137dc6eacba0e0f7b50508a1cadac9
Norbu Tsering <ntsering91@gmail.com>
2017-08-02 17:15:48 -0400

    Updated example game code to reflect changes to the app module

--- examples/tbs_square/game.py
+++ examples/tbs_square/game.py
@@ -2,10 +2,10 @@ if __name__ == '__main__':
     import engine as en
     import constants as cn
     from test_scene import TestScene
-    app = en.app.App(cn.WINDOW_SIZE,
+    en.app.initialize(cn.WINDOW_SIZE,
         cn.WINDOW_CAPTION,
         cn.SCALE,
         cn.SHOW_FPS)
 
-    app.push_scene(TestScene())
-    app.main_loop()
+    en.app.push_scene(TestScene())
+    en.app.main_loop()
