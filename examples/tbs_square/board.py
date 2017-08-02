0803259f18a399cdf7e044dcb34443fd21614d15
Norbu Tsering <ntsering91@gmail.com>
2017-08-02 00:36:02 -0400

    Added examples directory with example game

---
+++ examples/tbs_square/board.py
@@ -0,0 +1,106 @@
+import engine as en
+import pygame as pg
+import constants as cn
+
+class InfoType:
+    MOVE=1
+    ATTACK=2
+
+class Board(en.entity.Entity):
+    def __init__(self, size, tiles=None):
+        self.tiles = tiles
+        width = size[0]
+        height = size[1]
+        width_in_px = width * cn.CELL_SIZE[0]
+        height_in_px = height  * cn.CELL_SIZE[1]
+        super(Board, self).__init__((0, 0), width_in_px, height_in_px)
+
+        self.width_in_cells = width
+        self.height_in_cells = height
+
+        self.selected_entity = None
+        self.info_type = InfoType.MOVE
+
+        en.pubsub.subscribe(self, "cell_click")
+        en.pubsub.subscribe(self, "selected")
+
+    def update(self, dt):
+        mouse_pos = en.mouse.get_position()
+        cell_x = mouse_pos[0] / cn.CELL_WIDTH
+        cell_y = mouse_pos[1] / cn.CELL_HEIGHT
+
+        if en.mouse.was_pressed(0):
+            self.info_type = InfoType.MOVE
+            clicked_cell = (cell_x, cell_y)
+
+            if cell_x < self.width_in_cells and cell_y < self.height_in_cells:
+                en.pubsub.publish("move_piece", self, (self.selected_entity, clicked_cell))
+                en.pubsub.publish("cell_click", self, clicked_cell)
+
+        if en.mouse.was_pressed(2):
+            self.info_type = InfoType.ATTACK
+            clicked_cell = (cell_x, cell_y)
+            en.pubsub.publish("cell_click", self, clicked_cell)
+
+
+    def draw(self):
+        surf = en.graphics.get_main_surface()
+        if self.tiles is None:
+            for x in xrange(self.width_in_cells+1):
+                pg.draw.line(surf, (255, 0, 0),
+                    (x*cn.CELL_WIDTH, 0),
+                    (x*cn.CELL_WIDTH, self.rect.bottom))
+
+            for y in xrange(self.height_in_cells+1):
+                pg.draw.line(surf, (255, 0, 0),
+                    (0, y*cn.CELL_HEIGHT),
+                    (self.rect.right, y*cn.CELL_HEIGHT))
+        else:
+            map_height = self.tiles['height']
+            map_width = self.tiles['width']
+            for i, tile in enumerate(self.tiles['data']):
+                x = (i % cn.BOARD_WIDTH) * cn.CELL_WIDTH
+                y = (i / cn.BOARD_WIDTH) * cn.CELL_WIDTH
+
+                rect = self.tiles['rects'][tile]
+                tile_img = self.tiles['image'].subsurface(rect)
+                en.graphics.draw_image(tile_img, (x, y))
+
+        # Draw available moves if an entity is selected
+        if self.selected_entity:
+            if self.info_type == InfoType.MOVE:
+                draw_rect = self.selected_entity.rect
+                pg.draw.rect(surf, (255, 0, 255),
+                    draw_rect, 2)
+
+                for cell in self.selected_entity.valid_move_cells:
+                    pg.draw.rect(surf, (0, 255, 0),
+                        (cell[0] * cn.CELL_WIDTH,
+                         cell[1] * cn.CELL_HEIGHT,
+                         cn.CELL_WIDTH, cn.CELL_HEIGHT),
+                         2)
+            elif self.info_type == InfoType.ATTACK:
+                draw_rect = self.selected_entity.rect
+                pg.draw.rect(surf, (255, 0, 255),
+                    draw_rect, 2)
+
+                for cell in self.selected_entity.valid_move_cells:
+                    pg.draw.rect(surf, (255, 0, 0),
+                        (cell[0] * cn.CELL_WIDTH,
+                         cell[1] * cn.CELL_HEIGHT,
+                         cn.CELL_WIDTH, cn.CELL_HEIGHT),
+                         4)
+
+
+    def notify(self, event, sender, data):
+        if event == "cell_click":
+            if not self.selected_entity:
+                return
+            if data != self.selected_entity.cell_position:
+                self.selected_entity = None
+
+        elif event == "selected":
+            if sender is self.selected_entity:
+                self.selected_entity = None
+            else:
+                self.selected_entity = sender
---
+++ examples/tbs_square/game.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    import engine as en
+    import constants as cn
+    from test_scene import TestScene
+    app = en.App(cn.WINDOW_SIZE,
+        cn.WINDOW_CAPTION,
+        cn.SCALE,
+        cn.SHOW_FPS)
+
+    app.push_scene(TestScene())
+    app.main_loop()
