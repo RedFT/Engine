bc6082493a80c95662f7b35fe7256fb43918e0ef
Norbu Tsering <ntsering91@gmail.com>
2017-08-02 21:53:36 -0400

    corrected typo

--- engine/entity.py
+++ engine/entity.py
@@ -5,7 +5,7 @@ class Entity(node.Node):
     def __init__(self, position):
         super(Entity, self).__init__()
         self.position = list(position)
-        self.camera_coordinate = position
+        self.camera_coordinates = position
 
     def update(self, dt):
         raise NotImplementedError
